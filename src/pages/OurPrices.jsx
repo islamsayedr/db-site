5b3014eeec57dc9c0d69b6a9a8e878664ff6f892
IslamSayed <islamsayedr@gmail.com>
2024-03-24 15:06:11 +0200

    ignore

--- src/pages/OurPrices.jsx
+++
@@ -1,5 +0,0 @@
-import Pricing from "../comp/Priceing";
-
-export default function OurPrices() {
-  return <Pricing />
-}
--- src/pages/OurProcess.jsx
+++
@@ -1,5 +0,0 @@
-import Process from "../comp/Process";
-
-export default function OurProcess() {
-  return <Process />;
-}
